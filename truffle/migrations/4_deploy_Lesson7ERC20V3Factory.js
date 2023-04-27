112e9fd53e17e3c8750ea0fe70b9367acff11370
Gaollg <gaollg@qq.com>
2023-04-27 12:22:42 +0800

    所有合约发布成功

--- truffle/migrations/4_deploy_Lesson7ERC20V3Factory.js
+++ truffle/migrations/4_deploy_Lesson7ERC20V3Factory.js
@@ -1,5 +1,5 @@
 const Lesson7ERC20V3Factory = artifacts.require('Lesson7ERC20V3Factory');
 
 module.exports = function (deployer) {
-  deployer.deploy(Lesson7ERC20V3Factory);
+  deployer.deploy(Lesson7ERC20V3Factory, '0x7522c532b511446a2387748b81eb5e73e2231c80', 0);
 };
